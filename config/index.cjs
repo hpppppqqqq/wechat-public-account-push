d7d0207f7a0211f28f1f9ef34b3c0ccfcf217af8
hpppppqqqq <117006050+hpppppqqqq@users.noreply.github.com>
2022-11-02 09:03:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'gh_f0c5af08e8911',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ocPMm6mLu8K2nkx-5LkOPVc7wbzE',
+      id: '0001',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '2hDb3WhCAvMmprQB_gPyYWic6lScf0lpc1XVq-2BlmU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
