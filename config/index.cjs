758b958a6f9187c37410845346be5b0ae92cde70
hpppppqqqq <117006050+hpppppqqqq@users.noreply.github.com>
2022-12-02 21:09:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: 'gh_f0c5af08e8911',
+      name: '小猫咪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '55c415d1b8a542aebd05195f93b8390f',
+      id: 'ocPMm6mLu8K2nkx-5LkOPVc7wbzE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-14',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -42,9 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2021-01-14' },
       ],
     },
   ],
